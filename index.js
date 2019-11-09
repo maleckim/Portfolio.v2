fccec66afd3ec436aaee6de83595bc5f6314d314
Matthew Malecki <maleckimatthewm@gmail.com>
2019-11-09 16:29:28 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+
+function homeScreen() {
+     
+        
+        $('.aboutPage').hide()
+        $('.contactPage').hide()
+        $('.projectsPage').hide()
+    
+}
+
+
+
+function navHandler() {
+    $('.home').on('click', function (e) {
+        $('.homePage').show()
+        $('.aboutPage').hide()
+        $('.contactPage').hide()
+        $('.projectsPage').hide()
+
+    });
+    $('.about').on('click', function (e) {
+        $('.aboutPage').show()
+        $('.homePage').hide()
+        $('.contactPage').hide()
+        $('.projectsPage').hide()
+
+    });
+    $('.contact').on('click', function (e) {
+        $('.aboutPage').hide()
+        $('.homePage').hide()
+        $('.contactPage').show()
+        $('.projectsPage').hide()
+
+    });
+    $('.projects').on('click', function (e) {
+        $('.aboutPage').hide()
+        $('.homePage').hide()
+        $('.contactPage').hide()
+        $('.projectsPage').show()
+
+    });
+
+}
+
+
+
+function render() {
+    $(homeScreen);
+    $(navHandler);
+}
+
+$(render);
